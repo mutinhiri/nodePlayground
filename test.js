cfae41ac7e19cf8211437255c938134c505c613e
Bunbee <barnabmutinhiri@gmail.com>
2021-11-10 15:51:44 +0200

    Add changes to test

--- test.js
+++ test.js
@@ -24,4 +24,14 @@ test(reverseString('hello'),()=>{
 })
 
 //task 3 
-
+function add(a, b) {
+  return a + b;
+}function subtract(a, b) {
+  return a - b;
+}function multiply(a, b) {
+  return a * b;
+}function divide(a, b) {
+  return a / b;
+}function modulo(a, b) {
+  return a % b;
+}
