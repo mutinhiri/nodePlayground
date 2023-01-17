9dda18dae37cccf1d23e7b5ea1776dc11b752c5f
Bunbee <barnabas@velocityinc.tech>
2023-01-17 22:56:31 +0200

    Add blog change

--- models/blog.js
+++ models/blog.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
     title: {
-        
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true 
     }
 })
