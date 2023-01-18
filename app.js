b549047f49e5a440bf77cbc9d7c7b418d4a89d6e
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2023-01-18 16:25:23 +0200

    Add changes to route

--- app.js
+++ app.js
@@ -83,7 +83,15 @@ app.get('/blogs', (req, res) => {
 })
 
 app.post('/blogs', (req, res) => {
-    console.log(req.body)
+    const blog = new Blog(req.body)
+
+    blog.save()
+      .then((result) => {
+        res.redirect('/blogs')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 })
 
 app.get('/blogs/create', (req, res) => {
