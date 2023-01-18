20fcd561f91d09c6d1e3cc03d67a20ecff7e97f9
Bunbee <barnabas@velocityinc.tech>
2023-01-18 08:03:19 +0200

    Import blog model

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-
+const Blog = require('./models/blog')
 
 // express app
 const app = express();
